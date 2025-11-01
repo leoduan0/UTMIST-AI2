11e068e280c9e1033e52cd73f5de05d7af7a4d43
Leo Duan <leoduan0@icloud.com>
2025-11-01 18:32:01 -0400

    train_agent.py: tweak rewards

--- user/train_agent.py
+++ user/train_agent.py
@@ -615,10 +615,10 @@ def gen_reward_manager():
         #'target_height_reward': RewTerm(func=base_height_l2, weight=0.0, params={'target_height': -4, 'obj_name': 'player'}),
         "danger_zone_reward": RewTerm(func=danger_zone_reward, weight=0.5),
         "damage_interaction_reward": RewTerm(
-            func=damage_interaction_reward, weight=1.0
+            func=damage_interaction_reward, weight=5.0
         ),
         #'head_to_middle_reward': RewTerm(func=head_to_middle_reward, weight=0.01),
-        #'head_to_opponent': RewTerm(func=head_to_opponent, weight=0.05),
+        "head_to_opponent": RewTerm(func=head_to_opponent, weight=5.0),
         "penalize_attack_reward": RewTerm(
             func=in_state_reward, weight=-0.04, params={"desired_state": AttackState}
         ),
@@ -628,19 +628,22 @@ def gen_reward_manager():
         #'taunt_reward': RewTerm(func=in_state_reward, weight=0.2, params={'desired_state': TauntState}),
     }
     signal_subscriptions = {
-        "on_win_reward": ("win_signal", RewTerm(func=on_win_reward, weight=50)),
+        "on_win_reward": ("win_signal", RewTerm(func=on_win_reward, weight=10.0)),
         "on_knockout_reward": (
             "knockout_signal",
-            RewTerm(func=on_knockout_reward, weight=8),
+            RewTerm(func=on_knockout_reward, weight=5.0),
         ),
-        "on_combo_reward": ("hit_during_stun", RewTerm(func=on_combo_reward, weight=5)),
+        "on_combo_reward": (
+            "hit_during_stun",
+            RewTerm(func=on_combo_reward, weight=2.0),
+        ),
         "on_equip_reward": (
             "weapon_equip_signal",
-            RewTerm(func=on_equip_reward, weight=10),
+            RewTerm(func=on_equip_reward, weight=4.0),
         ),
         "on_drop_reward": (
             "weapon_drop_signal",
-            RewTerm(func=on_drop_reward, weight=15),
+            RewTerm(func=on_drop_reward, weight=4.0),
         ),
     }
     return RewardManager(reward_functions, signal_subscriptions)
